3f3ba3c546c007643fa5857f121eefa42d1fc0db
unknown <AliceLiu1218>
2023-03-25 16:45:50 -0400

    populate next question after selecting answer

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ var answer2 = document.querySelector("#answer2")
 var answer3 = document.querySelector("#answer3")
 var answer4 = document.querySelector("#answer4")
 var result = document.querySelector("#result")
+var question = document.querySelector("#questiontxt")
 
 var ans1Index = answer1.dataset.index;
 var ans2Index = answer2.dataset.index;
@@ -35,26 +36,51 @@ function jumppage() {
 }
 startbtn.addEventListener("click", timerSetup)
 
+function populateNextQuestion2() {
+    
+    question.textContent = "question22222";
+    answer1.textContent = "ANSWER222222222";
+    answer2.textContent = "ANSWER222222222";
+    answer3.textContent = "ANSWER222222222";
+    answer4.textContent = "ANSWER2222222222";
 
-function setAns1() {
+}
+function populateNextQuestion3() {
+    
+    question.textContent = "question333333";
+    answer1.textContent = "ANSWER33333333333";
+    answer2.textContent = "ANSWER3333333333";
+    answer3.textContent = "ANSWER33333333333";
+    answer4.textContent = "ANSWER333333333333";
+}
+function populateNextQuestion4() {
+    
+    question.textContent = "question4444444444";
+    answer1.textContent = "ANSWER44444444444";
+    answer2.textContent = "ANSWER4444444444";
+    answer3.textContent = "ANSWER444444444444";
+    answer4.textContent = "ANSWER4444444444444";
+}
+
+function setAns1st() {
     
     localStorage.setItem("selected", ans1Index);
     compareAns = localStorage.getItem("selected");
     checkAns();
 }
-function setAns2() {
+function setAns2nd() {
     
     localStorage.setItem("selected", ans2Index);
     compareAns = localStorage.getItem("selected");
     checkAns();
 }
-function setAns3() {
+function setAns3rd() {
     
     localStorage.setItem("selected", ans3Index);
     compareAns = localStorage.getItem("selected");
     checkAns();
 }
-function setAns4() {
+function setAns4th() {
     
     localStorage.setItem("selected", ans4Index);
     compareAns = localStorage.getItem("selected");
@@ -63,13 +89,14 @@ function setAns4() {
 function checkAns() {
     if (compareAns == "1") {
         result.textContent = "Correct!";
-        populateNextQuestion();
+        populateNextQuestion2();
     }else {
         result.textContent = "Wrong!";
-        populateNextQuestion();
+        populateNextQuestion2();
     }
 }
-answer1.addEventListener("click",setAns1)
-answer2.addEventListener("click",setAns2)
-answer3.addEventListener("click",setAns3)
-answer4.addEventListener("click",setAns4)
+answer1.addEventListener("click",setAns1st)
+answer2.addEventListener("click",setAns2nd)
+answer3.addEventListener("click",setAns3rd)
+answer4.addEventListener("click",setAns4th)
+
