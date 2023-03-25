914e1235bd402aaa58f564ef41eaf4a0248daccc
unknown <AliceLiu1218>
2023-03-25 14:47:18 -0400

    achieve jump page funtionality to display question after press start

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 var startbtn = document.querySelector("#startbtn")
 var timer = document.querySelector("#timer")
 var count = 5;
-
+var startpage = document.querySelector(".startpage")
+var questionpage = document.querySelector(".question")
 
 //set up timer
 function timerSetup() {
+    jumppage();
     var myinterval = setInterval(function() {
         count--;
         timer.textContent = `Timer: ${count}`
@@ -16,6 +18,8 @@ function timerSetup() {
     
 }
 
-
-
+function jumppage() {
+    startpage.setAttribute("style","display:none");
+    questionpage.setAttribute("style","display:block");
+}
 startbtn.addEventListener("click", timerSetup)
